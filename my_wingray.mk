35915dea2fd4b3a6029fbe4c7d2b59407ff083e4
Chia-Wei Li <dreamcwli@gmail.com>
2012-08-10 12:54:45 +0800

    Add my_wingray build
    Signed-off-by: Chia-Wei Li <dreamcwli@gmail.com>

---
+++ my_wingray.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/moto/wingray/full_wingray.mk)
+
+PRODUCT_NAME := my_wingray
+PRODUCT_DEVICE := wingray
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Xoom
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_BUILD_PROPERTY_OVERRIDES += \
+    PRODUCT_NAME=tervigon \
+    PRIVATE_BUILD_DESC="tervigon-user 4.1.1 JRO03H 405518 release-keys" \
+    BUILD_FINGERPRINT="motorola/tervigon/wingray:4.1.1/JRO03H/405518:user/release-keys"
